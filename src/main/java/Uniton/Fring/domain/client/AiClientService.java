ec1053bf016aa246db5a2469be0c4ad2296d180d
d6nggyun <d6nggyun@gmail.com>
2025-06-26 22:03:13 +0900

    fix: Backend URL 주입 삭제

--- src/main/java/Uniton/Fring/domain/client/AiClientService.java
+++ src/main/java/Uniton/Fring/domain/client/AiClientService.java
@@ -12,7 +12,6 @@ import Uniton.Fring.global.exception.ErrorCode;
 import Uniton.Fring.global.security.jwt.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,9 +25,6 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Slf4j
 public class AiClientService {
-
-    @Value("${backend.base-url}")
-    private String backendUrl;
 
     private final AiClient aiClient;
     private final ProductRepository productRepository;
