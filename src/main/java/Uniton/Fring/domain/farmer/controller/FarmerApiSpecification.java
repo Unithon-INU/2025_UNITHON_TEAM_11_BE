81ae592411aea8910e8c4483e8d1c7eaf71247c5
d6nggyun <d6nggyun@gmail.com>
2025-06-27 04:17:07 +0900

    fix: 스토어 정보 수정 스웨거 - #16

--- src/main/java/Uniton/Fring/domain/farmer/controller/FarmerApiSpecification.java
+++ src/main/java/Uniton/Fring/domain/farmer/controller/FarmerApiSpecification.java
@@ -1,6 +1,8 @@
 package Uniton.Fring.domain.farmer.controller;
 
+import Uniton.Fring.domain.farmer.dto.req.UpdateStoreRequestDto;
 import Uniton.Fring.domain.farmer.dto.res.StoreItemsResponseDto;
+import Uniton.Fring.domain.farmer.dto.res.StoreResponseDto;
 import Uniton.Fring.domain.product.dto.res.SimpleProductResponseDto;
 import Uniton.Fring.global.security.jwt.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,9 +10,12 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "Farmer", description = "농부 전용 페이지 API")
 public interface FarmerApiSpecification {
@@ -30,4 +35,21 @@ public interface FarmerApiSpecification {
     )
     ResponseEntity<StoreItemsResponseDto<SimpleProductResponseDto>> getStore(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                              @RequestParam(defaultValue = "0") int page);
+
+    @Operation(
+            summary = "스토어 정보 수정",
+            description = "판매자의 스토어 정보를 수정합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            description = "스토어 정보 수정 성공",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = StoreResponseDto.class)
+                            )
+                    )
+            }
+    )
+    ResponseEntity<StoreResponseDto> updateStore(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                 @RequestPart @Valid UpdateStoreRequestDto updateStoreRequestDto,
+                                                 @RequestPart(value = "image", required = false) MultipartFile image);
 }
